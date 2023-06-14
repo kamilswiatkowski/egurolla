30eb203975ea06bb38fd4d59bbe25187a57ed550
kamilswiatkowski <kamil.swiatkowski@tdsoft.pl>
2023-06-14 20:13:27 +0200

    about agustin

--- wp-content/themes/wp-eds/includes/functions/acf-block.php
+++ wp-content/themes/wp-eds/includes/functions/acf-block.php
@@ -17,6 +17,18 @@ function my_acf_init()
             'post_types'      => ['page', 'post'],
           ]
         );
+        acf_register_block(
+          [
+            'name'            => 'about-agustin',
+            'title'           => __('O Agustin Egurrola'),
+            'render_callback' => 'my_acf_block_render_callback',
+            'category'        => 'formatting',
+            'icon'            => 'admin-comments',
+            'align'           => 'full',
+            'keywords'        => ['o', 'agustin', 'egurrola'],
+            'post_types'      => ['page', 'post'],
+          ]
+        );
     }
 }
 
