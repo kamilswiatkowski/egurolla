eb13e993a4a63721ec75468262294e3d799a29c1
Kamil <kamil.pawleta@tdsoft.pl>
2023-06-17 00:56:08 +0200

    block: mapa

--- wp-content/themes/wp-eds/includes/functions/acf-block.php
+++ wp-content/themes/wp-eds/includes/functions/acf-block.php
@@ -77,6 +77,18 @@ function my_acf_init()
 				    'post_types'      => ['page', 'post'],
 			    ]
 		    );
+		    acf_register_block(
+			    [
+				    'name'            => 'map',
+				    'title'           => __('Mapa'),
+				    'render_callback' => 'my_acf_block_render_callback',
+				    'category'        => 'formatting',
+				    'icon'            => 'admin-comments',
+				    'align'           => 'full',
+				    'keywords'        => ['mapa'],
+				    'post_types'      => ['page', 'post'],
+			    ]
+		    );
     }
 }
 
