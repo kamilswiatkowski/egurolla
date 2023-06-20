61d5a22b2691278e4c58433faa54f16b4910a729
Likenessigor <info@likeness.pl>
2023-06-21 00:14:05 +0200

    casting-form draft

---
+++ wp-content/themes/wp-eds/partials/block/content-casting-form.php
@@ -0,0 +1,13 @@
+<?php
+
+$fields = [
+  'title' => get_field('title'),
+  'data' => get_field('data'),
+  'location' => get_field('location'),
+  'group'  => get_field('group'),
+  'desc'  => get_field('desc'),
+  'image' => get_field('image'),
+];
+?>
+
+
