dcecbb9cd48390a7f9d68b345ecbb43ab507a679
Kamil <kamil.pawleta@tdsoft.pl>
2023-06-28 22:52:44 +0200

    block: why us desktop + mobile

--- wp-content/themes/wp-eds/partials/block/content-why-choose-us.php
+++ wp-content/themes/wp-eds/partials/block/content-why-choose-us.php
@@ -1,27 +1,51 @@
 <?php
-
+		
+		$title = get_field('title');
+		$subtitle = get_field('subtitle');
+		$blocks = get_field('blocks');
+		$button = get_field('button');
 
 ?>
 <section class="why-us">
 		<div class="container">
 				<div class="row">
 						<div class="col-12">
-								<div class="red-title-italic red-title-italic--white">DLACZEGO WARTO Z NAMI JECHAĆ?</div>
-								<div class="red-subtitle red-subtitle--white">Przekonaj się, ile korzyści
-										może dać Ci taniec! </div>
+								<div class="red-title-italic red-title-italic--white"><?php echo $title; ?></div>
+								<div class="red-subtitle red-subtitle--white"><?php echo $subtitle; ?></div>
 						</div>
 				</div>
 				<div class="row why-us__wrapper">
 						<div class="col-12 col-md-6">
-								<div class="why-us__accordion">
-										<div class="why-us__top" data-accordion><div class="plus">+</div>Wieloletnie doswiadczenie<?php echo Like\get_svg('arrow-red-bottom'); ?></div>
-										<div class="why-us__bottom">Egurrola Dance Camp organizujemy już od ponad 20 lat. Dzięki wieloletniemu doświadczeniu jesteśmy w stanie zadbać o każdy, nawet najmniejszy szczegół, tak aby
-												w czasie campa niczego Ci nie zabrakło!</div>
-								</div>
+								<?php foreach ($blocks as $key => $block) {
+										if($key % 2 === 0 || $key === 0) { ?>
+												<div class="why-us__accordion">
+														<div class="why-us__top" data-accordion>
+																<div class="plus">+</div><?php echo $block['main_text']; ?><?php echo Like\get_svg(
+																	'arrow-red-bottom'
+																); ?></div>
+														<div class="why-us__bottom"><?php echo $block['content']; ?></div>
+												</div>
+										<?php } }?>
 						</div>
 						<div class="col-12 col-md-6">
-						
+								<?php foreach ($blocks as $key => $block) {
+										if($key % 2 !== 0) { ?>
+												<div class="why-us__accordion">
+														<div class="why-us__top" data-accordion>
+																<div class="plus">+</div><?php echo $block['main_text']; ?><?php echo Like\get_svg(
+																	'arrow-red-bottom'
+																); ?></div>
+														<div class="why-us__bottom"><?php echo $block['content']; ?></div>
+												</div>
+										<?php }
+								} ?>
 						</div>
 				</div>
-		</div>
+				<div class="row">
+						<div class="col-12">
+								<div class="why-us__btn">
+										<a href="<?php echo $button['url']; ?>" class="btn btn--red btn--white-bg"><?php echo $button['title']; ?></a>
+								</div>
+						</div>
+				</div>
 </section>
