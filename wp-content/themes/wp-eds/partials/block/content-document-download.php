c4a0b864fa31ac1dbe40a59ef6b7d3a1002f44fc
Likenessigor <info@likeness.pl>
2023-07-02 02:40:17 +0200

    document-download pc-mob-acf

--- wp-content/themes/wp-eds/partials/block/content-document-download.php
+++ wp-content/themes/wp-eds/partials/block/content-document-download.php
@@ -3,25 +3,31 @@ $fields = [
   'title' => get_field('title'),
   'attachment' => get_field('attachment'),
   'doc_name' => get_field('doc_name'),
- 
 ];
 ?>
 
+<?php
+$documents = get_field('document_download'); // Pobranie wartości z acf "document_download"
+?>
+
 <section class="document-download">
-
-<?php echo $fields['title']; ?>
-
-    <div class="container">
-        <div class="row d-lg-flex align-items-lg-center">
-
-        <?php $attachment = get_field('attachment'); ?>
-        <?php $doc_name = get_field('doc_name'); ?>
-
-        <?php if ($attachment && $doc_name) : ?>
-            <div class="col-lg-3 col-12"><a href="<?php echo $attachment['url']; ?>" target="_blank"><?php echo $doc_name; ?></a></div>
+  <div class="container">
+    <h3 class="red-title-italic heading"><?php echo $fields['title']; ?></h3>
+    <div class="row d-lg-flex align-items-lg-center">
+        <?php if ($documents) : ?>
+            <?php foreach ($documents as $document) : ?>
+            <?php $attachment = $document['attachment']; ?>
+            <?php $doc_name = $document['doc_name']; ?>
+            <?php if ($attachment && $doc_name) : ?>
+                <div class="col-lg-4 col-12">
+                  <div class="document-download__filebox"> 
+                    <?php echo Like\get_svg('download'); ?>
+                    <a href="<?php echo $attachment['url']; ?>" target="_blank" class="document-download__filebox-link"><?php echo $doc_name; ?></a>
+                  </div>
+                </div>
+            <?php endif; ?>
+            <?php endforeach; ?>
         <?php endif; ?>
-
-        </div>
     </div>
-    
+  </div>
 </section>
