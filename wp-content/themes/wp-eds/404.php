d403ee07d9c4ab82071cb1a710dd0939abf5f5d1
Likenessigor <info@likeness.pl>
2023-07-07 00:53:59 +0200

    404-fix-mobile

--- wp-content/themes/wp-eds/404.php
+++ wp-content/themes/wp-eds/404.php
@@ -3,25 +3,25 @@ get_header();
 ?>
 
 <?php
-$title = get_field('404_headline', 'option');
-$label = get_field('404_subtitle', 'option');
-$image = get_field('404_image', 'option');
-
+$fields = [
+'title' => get_field('404_headline', 'option'),
+'label' => get_field('404_subtitle', 'option'),
+'image' => get_field('404_image', 'option'),
+];
 ?>
 
-<section class="about-augstin 404">
+<section class="about-augstin error-404">
     <div class="container">
         <div class="row">
             <div class="col-12 d-md-flex align-items-md-center">
-                <img src="<?php echo $fields['image']; ?>" alt="" class="about-agustin__main-img">
+            
+            <img src="<?php echo $fields['image']; ?>" alt="" class="about-agustin__main-img">
                 <div class="about-agustin__inner">
-                    <span class="404">404</span>
-                    <?php echo Like\get_section_header(
-                      $fields['title'],
-                      $fields['label'],
-                      ''
-                    ); ?>
-                    <div class="about-agustin__btn-wrapper">
+                <span class="section__404">404</span>
+                    
+                    <p class="section__label"><?php echo $fields['title']; ?><span class="section__title"><?php echo $fields['label']; ?></span></p>
+                    
+                    <div class="button">
                             <a href="<?php echo esc_url( home_url( '/' ) ); ?>"
                                class="btn btn--white btn--red-bg">Przejdz na stronę główną</a>
                     </div>
