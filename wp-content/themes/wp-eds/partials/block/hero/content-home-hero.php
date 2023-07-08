4af4c2d85e4363dde40170a3ebf5bdb3f6d6b048
Kamil <kamil.pawleta@tdsoft.pl>
2023-07-08 09:02:28 +0200

    add yt video to bg

--- wp-content/themes/wp-eds/partials/block/hero/content-home-hero.php
+++ wp-content/themes/wp-eds/partials/block/hero/content-home-hero.php
@@ -5,10 +5,21 @@
 		$background = get_field('background_image');
 		$show_search = get_field('show_search');
 		$show_bottom_arrow = get_field('show_bottom_arrow');
+		$yt_url = get_field('youtube_url');
 ?>
 
 <section class="home-hero <?php echo $height = $show_bottom_arrow ? 'wide' : 'low'; ?>"
          style="background: url('<?php echo $background['url']; ?>') center center no-repeat;">
+		<?php if($yt_url) { ?>
+			<div class="home-hero__video-wrapper">
+					<div class="home-hero__video">
+							<iframe width="100%" height="100%" src="<?php echo $yt_url; ?>?mute=1&autoplay=1&controls=0&showinfo=0&rel=0&loop=1" frameborder="0"
+							        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							        allowfullscreen></iframe>
+					</div>
+			</div>
+	
+		<?php } ?>
 		<div class="container">
 				<div class="row">
 						<div class="col-12">
