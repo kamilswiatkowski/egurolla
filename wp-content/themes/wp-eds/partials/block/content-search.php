83946a2e1f4faee46a85a2d7512a4605a075b3a2
Kamil <kamil.pawleta@tdsoft.pl>
2023-07-08 11:23:43 +0200

    search desktop

--- wp-content/themes/wp-eds/partials/block/content-search.php
+++ wp-content/themes/wp-eds/partials/block/content-search.php
@@ -46,5 +46,35 @@
 						
 						</div>
 				</div>
+				<div class="row">
+						<div class="search__wrapper">
+						   <div class="search__wrapper-item">
+								   <div class="search__wrapper-item-title">Grupa Wiekowa</div>
+								   <div class="search__wrapper-item-subtitle">Juniorzy 13-16</div>
+						   </div>
+								<div class="search__wrapper-item">
+										<div class="search__wrapper-item-title">Styl</div>
+										<div class="search__wrapper-item-subtitle">Wybierz styl tańca</div>
+								</div>
+								<div class="search__wrapper-item">
+										<div class="search__wrapper-item-title">Instruktor</div>
+										<div class="search__wrapper-item-subtitle">Wybierz instruktora</div>
+								</div>
+								<div class="search__wrapper-item">
+										<div class="search__wrapper-item-title">Poziom</div>
+										<div class="search__wrapper-item-subtitle">Wybierz poziom uczestnika</div>
+								</div>
+								<div class="search__wrapper-item">
+										<div class="search__wrapper-item-title">Dzień</div>
+										<div class="search__wrapper-item-subtitle">Wybierz dzień/dni</div>
+								</div>
+								<div class="search__btn">
+										<?php echo Like\get_svg(
+											'loupe'
+										); ?>
+										SZUKAJ
+								</div>
+						</div>
+				</div>
 		</div>
 </section>
