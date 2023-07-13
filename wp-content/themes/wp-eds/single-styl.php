834f8e4adb65772e8b5e83575b99e9ecc36be4c1
kamilswiatkowski <kamil.swiatkowski@tdsoft.pl>
2023-07-13 22:46:40 +0200

    small fixes

---
+++ wp-content/themes/wp-eds/single-styl.php
@@ -0,0 +1,11 @@
+<?php
+
+$post = get_post();
+$content = $post->post_content;
+get_header();
+
+while (have_posts()) : the_post();
+    echo apply_filters('the_content', $content);
+endwhile;
+
+get_footer();
